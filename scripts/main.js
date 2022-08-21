4b30fcde8569024c4d411bb5c6b7f2ab82ad3c97
HMYグループ㍿ <111698169+HMYgroup@users.noreply.github.com>
2022-08-22 01:15:46 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/My test image.png') {
+    myImage.setAttribute ('src','images/firefox2.png');
+  } else {
+    myImage.setAttribute ('src','images/My test image.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('あなたの名前を入力してください。');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'ひとくち水ようかんのサイト、 ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'HMYグループ㍿の公式サイト、 ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
